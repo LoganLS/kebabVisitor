20edbc7b01e2935fab1fa7347a36a1fb56ac4b33
logoo_000 <logoo_000@164.81.243.172>
2017-12-07 10:51:31 +0100

    Mise en place de l'arborescence du visiteur de régime végétarien

---
+++ src/main/java/fr/unilim/iut/visitorkebab/VisiteurDeRegime.java
@@ -0,0 +1,27 @@
+package fr.unilim.iut.visitorkebab;
+
+import fr.unilim.iut.visitorkebab.ingredients.*;
+
+public interface VisiteurDeRegime {
+	public boolean visit(Agneau ingredientAgneau);
+	
+	public boolean visit(Boeuf ingredientBoeuf);
+	
+	public boolean visit(Crevette ingredientCrevette);
+	
+	public boolean visit(Fromage ingredientFromage);
+	
+	public boolean visit(Oignon ingredientOignon);
+	
+	public boolean visit(Pain ingredientPain);
+	
+	public boolean visit(Salade ingredientSalade);
+	
+	public boolean visit(Sauce ingredientSauce);
+	
+	public boolean visit(Thon ingredientThon);
+	
+	public boolean visit(Tomate ingredientTomate);
+	
+	public boolean visit(Assiette ingredientAssiette);
+}
---
+++ src/main/java/fr/unilim/iut/visitorkebab/VisiteurDeRegimeVegetarien.java
@@ -0,0 +1,49 @@
+package fr.unilim.iut.visitorkebab;
+
+import fr.unilim.iut.visitorkebab.ingredients.*;
+
+public class VisiteurDeRegimeVegetarien implements VisiteurDeRegime{
+	public boolean visit(Agneau ingredientAgneau){
+		return false;
+	}
+	
+	public boolean visit(Boeuf ingredientBoeuf){
+		return false;
+	}
+	
+	public boolean visit(Crevette ingredientCrevette){
+		return false;
+	}
+	
+	public boolean visit(Fromage ingredientFromage){
+		return false;
+	}
+	
+	public boolean visit(Oignon ingredientOignon){
+		return true;
+	}
+	
+	public boolean visit(Pain ingredientPain){
+		return true;
+	}
+	
+	public boolean visit(Salade ingredientSalade){
+		return true;
+	}
+	
+	public boolean visit(Sauce ingredientSauce){
+		return false;
+	}
+	
+	public boolean visit(Thon ingredientThon){
+		return false;
+	}
+	
+	public boolean visit(Tomate ingredientTomate){
+		return true;
+	}
+	
+	public boolean visit(Assiette ingredientAssiette){
+		return false;
+	}
+}
